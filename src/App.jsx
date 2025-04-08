a83ae5c31a4f79185b4901110f7c9df264e0ed92
sadra52 <sadraddin1375@gmail.com>
2025-04-08 21:27:06 +0330

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <p className="text-3xl font-bold underline text-blue-500">
+                hello world!
+            </p>
+        </>
+    )
+}
+
+export default App
